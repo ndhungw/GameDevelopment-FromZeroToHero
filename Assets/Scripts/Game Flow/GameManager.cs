5b671e868bdd5a3ff6157dcea4e03960fdf20ea2
buidohuy99 <sunnyhill93@gmail.com>
2021-01-09 23:27:10 +0700

    Initialize Game Menu, please ignnore loading scene UI. Loading scene have to be made ONE separate Scene, not just an overlay

---
+++ Assets/Scripts/Game Flow/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager GM;
+
+    private void Awake()
+    {
+        if (GM == null)
+        {
+            GM = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>(); 
+        }
+    }
+}
