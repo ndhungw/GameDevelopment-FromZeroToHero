139dcdfab91bc5fc3a7f85e4b769daa48a2dd848
johnnyshi1999 <johnnyshi99@gmail.com>
2021-01-19 11:22:15 +0700

    add script to health bar and GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance = null;
+
+    private int money = 1000;
+    private int food = 0;
+
+    public int Money { get { return money; } }
+    public int Food { get { return food; } }
+
+    public void ChangeMoney(int amount)
+    {
+        if (amount > money)
+        {
+            return;
+        }
+        money += amount;
+    }
+
+    public int ChangeFood(int amount) 
+    {
+        food = food + amount;
+
+        if (food < 0)
+        {
+            int owed = food;
+            food = 0;
+            return owed;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    private GameManager() { }
+
+    private void OnEnable()
+    {
+        Debug.Log(GameManager.GetInstance());
+    }
+
+    public static GameManager GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new GameManager();
+
+            var singletonObject = new GameObject();
+            instance = singletonObject.AddComponent<GameManager>();
+            singletonObject.name = typeof(GameManager).ToString() + " (Singleton)";
+
+            // Make instance persistent.
+            DontDestroyOnLoad(singletonObject);
+        }
+        return instance;
+    }
+
+
+}
---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HealthBar : MonoBehaviour
+{
+    public static HealthBar instance { get; private set; }
+
+    public Image Mask;
+
+    public Image Avatar;
+
+    float originalSize;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        originalSize = Mask.rectTransform.rect.width;
+    }
+
+    public void SetValue(float value)
+    {
+        Mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * value);
+    }
+}
